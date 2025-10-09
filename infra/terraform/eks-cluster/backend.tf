e0f91b46d76dd331e1afba6f6cf1043ba943019f
UdayParkar <udayparkar2003@gmail.com>
2025-10-09 14:41:37 +0530

    some changes

--- infra/terraform/eks-cluster/backend.tf
+++ infra/terraform/eks-cluster/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "todo-tfstate-4a9418"
+    bucket         = "todo-tfstate-df50f0"
     key            = "eks-cluster/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "todo-tf-lock"
--- infra/terraform/eks-cluster/main.tf
+++ infra/terraform/eks-cluster/main.tf
@@ -23,6 +23,16 @@ module "vpc" {
 
   enable_nat_gateway = true
   single_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
 }
 
 # ---- EKS Cluster ----
